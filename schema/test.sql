6286d2c2463b57f5c79b9ab08a5e8793818b87da
Arashi <edisonrhuang@vt.edu>
2024-04-16 17:09:46 -0400

    better test script format

--- schema/test.sql
+++ schema/test.sql
@@ -1,15 +1,29 @@
+/* To run, highlight each individual query */
+
+/* ======================
+ * === Useful Queries ===
+ * ======================
+ */
+
+/* Query the candidate table */
 SELECT * FROM candidate;
-SELECT * FROM skill;
-SELECT * FROM employer;
-SELECT * FROM authentication;
 
-SELECT * FROM candidate_skill;
-
-SELECT * FROM candidate NATURAL JOIN candidate_skill NATURAL JOIN skill;
-
-/* Query all candidates and join with skills, if any */
+/* Query the candidate + skill table */
 SELECT c.*, GROUP_CONCAT(s.skill_name) AS skills
 FROM candidate c
 JOIN candidate_skill cs ON c.candidate_id = cs.candidate_id
 JOIN skill s on cs.skill_id = s.skill_id
 GROUP BY c.candidate_id;
+
+/* Query the skill table */
+SELECT * FROM skill;
+
+/* Query the candidate_skill table */
+SELECT * FROM candidate_skill;
+
+/* Query the employer table */
+SELECT * FROM employer;
+
+/* Query the authentication table */
+SELECT * FROM authentication;
+
