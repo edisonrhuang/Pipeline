b268663bbc551d0e76ca7250dbfc2076e4235a40
Arashi <edisonrhuang@vt.edu>
2024-04-16 19:49:10 -0400

    script fix

--- schema/test.sql
+++ schema/test.sql
@@ -8,11 +8,11 @@
 /* Query the candidate table */
 SELECT * FROM candidate;
 
-/* Query the candidate + skill table */
+/* Query the candidate table with skills */
 SELECT c.*, GROUP_CONCAT(s.skill_name) AS skills
 FROM candidate c
-JOIN candidate_skill cs ON c.candidate_id = cs.candidate_id
-JOIN skill s on cs.skill_id = s.skill_id
+LEFT JOIN candidate_skill cs ON c.candidate_id = cs.candidate_id
+LEFT JOIN skill s on cs.skill_id = s.skill_id
 GROUP BY c.candidate_id;
 
 /* Query the skill table */
