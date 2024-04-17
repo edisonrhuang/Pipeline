7131b90518865711c6d6e067fd639d8fe0e94d0e
Arashi <edisonrhuang@vt.edu>
2024-04-17 18:09:05 -0400

    create user functionality

--- backend/src/db/queries/authenticationQueries.js
+++ backend/src/db/queries/authenticationQueries.js
@@ -1,4 +1,6 @@
 const connection = require('../connection');
+const { createCandidate } = require('./candidateQueries');
+const { createEmployer } = require('./employerQueries');
 
 /**
  * Retrieves user information from the database based on the provided email address.
@@ -14,7 +16,7 @@ const connection = require('../connection');
 function getUserInfo(email, callback) {
     connection.query('SELECT * FROM authentication WHERE email = ?', email, (err, res) => {
         if (err) {
-            console.error('Error fetching user with email ', emailmail, ': ', err);
+            console.error('Error fetching user with email ', email, ': ', err);
             return callback(err, null);
         }
 
@@ -42,13 +44,73 @@ function getUserInfo(email, callback) {
                 return callback(null, res);
             });
         }
-        // If the user type is neither "Candidate" nor "Employer"
+        // If no user was found with the provided email address
         else {
-            return console.error('Error: Could not find user with email ', authEmail, ' in the database.');
+            console.error('Error: Could not find user with email ', authEmail, ' in the database.');
+            return callback(null, undefined);
+        }
+    });
+}
+
+/**
+ * Creates a new user in the authentication table, along with the corresponding
+ * user in the candidate or employer table.
+ * 
+ * @param {string} email The email address of the user.
+ * @param {string} userType The type of user ('Candidate' or 'Employer').
+ * @param {object} info Additional information specific to the user type.
+ * @param {*} callback 
+ * A callback function to handle the result of the database query.
+ *   The callback follows the standard Node.js pattern: (err, result) => {...}
+ * - If an error occurs during the query execution, `err` will contain the error 
+ *   object.
+ * - If the query is successful, `res` will contain the fetched candidates.
+ */
+function createUser(email, userType, info, callback) {
+    connection.query('INSERT INTO authentication (email, user_type) VALUES (?, ?)', [email, userType], (err, res) => {
+        if (err) {
+            console.error('Error creating user with email ', email, ': ', err);
+            return callback(err, null);
+        }
+
+        if (userType == 'Candidate') {
+            createCandidate(info, (err, res) => {
+                if (err) {
+                    console.error('Error creating candidate with email ', email, ': ', err);
+                    return callback(err, null);
+                }
+                
+                const candidate_id = res.insertId;
+                connection.query('UPDATE authentication SET candidate_id = ? WHERE email = ?', [candidate_id, email], (err, res) => {
+                    if (err) {
+                        console.error('Error updating user_id for candidate with email ', email, ': ', err);
+                        return callback(err, null);
+                    }
+                    return callback(null, res);
+                });
+            });
+        } 
+        else if (userType == 'Employer') {
+            createEmployer(info, (err, res) => {
+                if (err) {
+                    console.error('Error creating employer with email ', email, ': ', err);
+                    return callback(err, null);
+                }
+                
+                const employer_id = res.insertId;
+                connection.query('UPDATE authentication SET employer_id = ? WHERE email = ?', [employer_id, email], (err, res) => {
+                    if (err) {
+                        console.error('Error updating user_id for employer with email ', email, ': ', err);
+                        return callback(err, null);
+                    }
+                    return callback(null, res);
+                });
+            });
         }
     });
 }
 
 module.exports = {
     getUserInfo,
+    createUser,
 }
