0f20711645fd26edd5ecc9066898653aab7b7aa9
Arashi <edisonrhuang@vt.edu>
2024-04-18 16:56:58 -0400

    more test scripts

--- schema/test.sql
+++ schema/test.sql
@@ -15,6 +15,18 @@ LEFT JOIN candidate_skill cs ON c.candidate_id = cs.candidate_id
 LEFT JOIN skill s on cs.skill_id = s.skill_id
 GROUP BY c.candidate_id;
 
+-- Query a candidate's connections
+SELECT e.*
+FROM connection cn
+JOIN employer e ON cn.employer_id = e.employer_id
+WHERE cn.candidate_id = ?; -- Change ? to an id
+
+-- Query a employer's connections
+SELECT c.*
+FROM connection cn
+JOIN candidate c ON cn.candidate_id = c.candidate_id
+WHERE cn.employer_id = ?; -- Change ? to an id
+
 /*
  * ===========================
  * === Less Useful Queries ===
