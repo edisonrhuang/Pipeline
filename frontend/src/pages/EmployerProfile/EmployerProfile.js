7478361c1bfe52b4de11a6f5a16fff34f536d5c0
Sandiroc <bsandilya1908@gmail.com>
2024-04-19 14:55:48 -0400

    employerprofile started

--- frontend/src/pages/EmployerProfile/EmployerProfile.js
+++ frontend/src/pages/EmployerProfile/EmployerProfile.js
@@ -1,8 +1,24 @@
+import React from 'react'
 import "./employerprofile.css"
-const EmployerProfile = () => {
+import image from '../../components/Assets/bivashPic.jpg';
+import Navbar from '../../components/LoginNavbar/LoginNavbar.js'
+
+const profileData = {
+     name: 'Bivash Oli',
+     schoolName: 'Virginia Tech',
+     gradDate: '2025',
+     major: 'Computer Science',
+     biography: 'I am a passionate software developer...',
+     skills: ['JavaScript', 'React.js', 'HTML', 'CSS', 'Node.js']
+   };
+
+
+const CandidateProfile = () => {
+     const { name, schoolName, gradDate, major, biography, skills } = profileData;
      return (
-               <div></div>
-          );
-     }
-     
-     export default EmployerProfile;
+          <body>
+          </body>
+     );
+}
+
+export default CandidateProfile;
